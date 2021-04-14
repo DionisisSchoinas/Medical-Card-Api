facc93a5079e7b8b159fc5736410dd6082074f65
Schoinas Dionisis <schinasdionisis@gmail.com>
2021-04-14 22:23:52 +0300

    Update 20210411123920_create_images.rb

--- db/migrate/20210411123920_create_images.rb
+++ db/migrate/20210411123920_create_images.rb
@@ -2,7 +2,7 @@ class CreateImages < ActiveRecord::Migration[6.1]
   def change
     create_table :images do |t|
       t.string :image_base64
-      t.references :doctor, null: true, foreign_key: true
+      t.references :doctor, null: false, foreign_key: true
 
       t.timestamps
     end
